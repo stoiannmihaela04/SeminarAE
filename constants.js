7a5a4cc3697ed6bc6b91fe60646f8ea622f8c979
stoiannmihaela04 <stoiannmihaela04@gmail.com>
2025-11-07 19:44:57 +0200

    exercised variables

---
+++ constants.js
@@ -0,0 +1,30 @@
+// var var1 =1;
+// let var2 = 2;
+// const var3 = 3;
+
+// var1 = 5;
+// console.log(var1);
+
+
+// var2 = 5;
+// console.log(var2);
+
+// var3 = 5;
+// console.log(var3);
+
+function testVariables(param) {
+    if (param === true) {
+        var var1 =1;
+        let var2 = 2;
+        const var3 = 3;
+    }
+
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+
+
+}
+
+
+testVariables(true);
